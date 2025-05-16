72c4a928c89544881473f0860e8aa376136b4822
jandirp24 <apalmacastellon@gmail.com>
2025-05-16 01:54:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const conexion = {
     host: process.env.MYSQLHOST,
